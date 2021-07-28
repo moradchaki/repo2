ef89047134b23a0d74a6eaf5d6b5c90c01b1203c
unknown <chmorad005@gmail.com>
2021-07-28 22:08:45 +0200

    first

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
+  <div class="about pa-6">
     <h1>This is an about page</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis, nemo! Quod facilis obcaecati quia, eum ratione autem omnis unde maiores molestiae consequatur id accusantium? Alias nisi numquam eveniet, inventore consequuntur veniam, accusantium error odio reiciendis nihil unde excepturi iste labore.</p>
+    <p>Cupiditate facilis saepe pariatur placeat et natus perspiciatis dolor doloremque. Quos et quas veritatis ab vitae temporibus, doloribus animi impedit nesciunt blanditiis culpa. Nemo non illum nihil totam aperiam optio, quibusdam numquam quaerat omnis natus quidem temporibus fuga maiores pariatur.</p>
+    <p>Recusandae repudiandae reiciendis inventore animi exercitationem hic fuga temporibus eum nihil. Quibusdam, voluptatum sapiente libero deleniti ullam recusandae voluptates odio, qui vitae necessitatibus illo quae culpa laudantium quasi numquam possimus nemo nisi obcaecati harum? Nobis obcaecati corporis laboriosam tempore temporibus.</p>
+    <p>Iusto nulla ratione deleniti, doloremque, suscipit delectus facilis possimus inventore consequatur dolores cum, exercitationem modi cumque quis sed corrupti cupiditate assumenda tenetur molestiae. Consequatur corporis aspernatur nobis laborum tempora nisi deserunt unde facilis cupiditate perferendis, consequuntur, labore voluptatem autem obcaecati!</p>
   </div>
 </template>
